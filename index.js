4211a3f2d75fcacd116b8bee0367ecde6f04678b
xiaoen-liu <149930882+xiaoen-liu@users.noreply.github.com>
2024-07-17 00:37:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-
+function extractScore(text) {
+    const startTag = "<code_review_score>";
+    const endTag = "</code_review_score>";
+    const startIndex = text.indexOf(startTag) + startTag.length;
+    const endIndex = text.indexOf(endTag);
+    if (startIndex !== -1 && endIndex !== -1) {
+        return parseInt(text.slice(startIndex, endIndex));
+    }
+    return null;
+}
 
 try {
   // Fetch the value of the input 'who-to-greet' specified in action.yml
